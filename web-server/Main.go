3becabbb7b00bb2d6f377a08acfc7c65a018dd55
Brian Toro <brianhorrors@hotmail.com>
2020-07-02 03:09:48 -0500

    Error control to listen server

--- web-server/Main.go
+++ web-server/Main.go
@@ -1,8 +1,13 @@
 package main
 
+import "fmt"
+
 func main() {
 	server := NewServer(":8000")
 	server.Handle("/", HandleRoot)
 	server.Handle("/api", server.addMiddleware(HandleHome, CheckInventory(), CheckAuth()))
-	server.Listen()
+	err := server.Listen()
+	if err != nil {
+		fmt.Println(err)
+	}
 }
